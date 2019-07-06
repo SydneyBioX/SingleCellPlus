e703d617fc1b2e537221588c78a3824c801c789c
kevinwang09 <kevin.wang@sydney.edu.au>
2019-07-06 15:55:59 +1000

    diagnose readRDS problem

--- install.R
+++ install.R
@@ -1,5 +1,17 @@
-# DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
-# use 'rake' to generate 'install.R'.
+# List the library paths
+# The issue is likely to be in the first directory
+paths = .libPaths()
+
+## Try and detect bad files
+list.files(paths, 
+           pattern = "^00LOCK*|*\\.rds$|*\\.RDS$",
+           full.names = TRUE)
+
+## List files of size 0
+l = list.files(paths, full.names = TRUE)
+l[sapply(l, file.size) == 0]
+
+###############################################
 
 if ("BiocManager" %in% rownames(installed.packages()))
 	remove.packages("BiocManager")
